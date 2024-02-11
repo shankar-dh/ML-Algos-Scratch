2bf9f861da1f90bbb2727e301c4495ef66baac3e
Dhanush Kumar <shankar.dh@northeastern.edu>
2024-02-11 13:06:55 -0500

    added linear regression code

--- Linear Regression/Linear_regression.py
+++ Linear Regression/Linear_regression.py
@@ -1,34 +1,47 @@
 import numpy as np
 
-class LinearRegression:
+class LinearRegression():
 
-    def __init__(self, X, y, iterations=10000, alpha=0.01):
-        self.iterations = iterations
-        self.alpha = alpha
-        self.weights = np.zeros((X.shape[1] + 1, 1))  # Include bias term in weights
-        self.loss = []
-        self.X = np.c_[np.ones((X.shape[0], 1)), X]  # Augment X with bias term
+    def __init__(self, verbose = False, n_iterations= 10000, learning_rate = 0.01,
+                  regularization = None, regularization_strength = 0):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+        self.regularization = regularization
+        self.regularization_strength = regularization_strength
+        self.verbose = verbose
 
-    def _mse(self, y_pred, y_actual):
-        return np.mean((y_pred - y_actual) ** 2)
 
-    def _fit(self):
-        return np.dot(self.X, self.weights)
+    def _compute_mse_loss(self, y_pred, y_actual):
+        return np.mean((y_actual - y_pred)**2)
+            
+    def fit(self, X,y):
+        self.weights = np.zeros((X.shape[1], 1))
+        self.bias = 0
+        
+        for i in range(self.n_iterations):
+            y_pred = np.dot(X, self.weights) + self.bias
 
-    def _compute_gradients(self, y_pred, y_actual):
-        return 2 / self.X.shape[0] * np.dot(self.X.T, (y_pred - y_actual))
+            # compute loss
+            loss = self._compute_mse_loss(y_pred, y)
+            if self.verbose:
+                print(f"Loss at iteration {i}:{loss}")
 
-    def _gradient_descent(self, Y):
-        for i in range(self.iterations):
-            y_pred = self._fit()
-            loss_value = self._mse(y_pred, Y)
-            self.loss.append(loss_value)
-            grad = self._compute_gradients(y_pred, Y)
-            self.weights -= self.alpha * grad
+            # update weights and biases
+            dw = (2/X.shape[0]) * (np.dot(X.T, (y_pred- y)))
+            db = (2 * np.mean(y_pred - y))
+            
+            if self.regularization == 'l1':
+                dw = dw + self.regularization_strength * np.sign(self.weights)
+            if self.regularization == 'l2':
+                dw = dw +  2 * self.regularization_strength * self.weights
 
-            if i % 10 == 0:
-                print("Loss after ", i, "iteration is: ", loss_value)
+            self.weights = self.weights - self.learning_rate *  dw
+            self.bias = self.bias - self.learning_rate * db
 
-    def _predict(self, X):
-        X_bias = np.c_[np.ones((X.shape[0], 1)), X]  # Augment X with bias term
-        return np.dot(X_bias, self.weights)
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
+        
+    
+
+
+
